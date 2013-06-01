fd0fbc3a688b464a8c8594a3aa7e8257107f1b59
Matthew Palmer <matt@palmer.im>
2013-06-01 10:38:20 +1000

    first commit - .pull works

---
+++ index.js
@@ -0,0 +1,50 @@
+var http = require('http');
+var fs = require('fs');
+var restify = require('restify');
+
+var server = restify.createServer();
+server.get('/', function(req, res) {
+  var stream = fs.createReadStream('index.html');
+  stream.on('error', function (err) {
+      res.statusCode = 500;
+      res.end(String(err));
+  });
+  stream.pipe(res);
+});
+server.get('/slender.min.js', function(req, res) {
+  console.log(res, req);
+  var stream = fs.createReadStream('slender.min.js');
+  stream.on('error', function (err) {
+      res.statusCode = 500;
+      res.end(String(err));
+  });
+  res.writeHead(200, {
+    'Content-Type':'text/javascript'
+  });
+  stream.pipe(res);
+});
+server.get('/:path', function(req, res) {
+  console.log(res, req);
+  var stream = fs.createReadStream(req.params.path);
+  stream.on('error', function (err) {
+      res.statusCode = 500;
+      res.end(String(err));
+  });
+  stream.pipe(res);
+});
+
+server.put('/file/:path', function(req, res) {
+  console.log(req, res); //should have data passed in
+  var data;
+  fs.writeFile("/file"+path, "Hey there!", function(err) {
+    if(err) {
+        console.log(err);
+    } else {
+        console.log("The file was saved!");
+    }
+}); 
+});
+
+server.listen(3000, function() {
+  console.log('%s listening at %s', server.name, server.url);
+});
